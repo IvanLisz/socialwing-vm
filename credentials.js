b1d48991379cb53f2dcd8d039d617c61f006e5bd
Iván Nicolás Liszewski <ivan.nicolas.liszewski@gmail.com>
2015-05-25 04:24:54 -0300

    app credentials

--- credentials.js
+++ credentials.js
@@ -1,4 +1,5 @@
 module.exports = {
-	APP_TOKEN: '7t5i7gjUnVpSAtaiTtapd4my3',
-	APP_SECRET: '93VG4qckXNp74V7JXLVnkhhNm9XpddmrASiUt1d3OsaTXvVLhu'
+	APP_TOKEN: 'KpwwTmOh0xNoeraalo71as7cC',
+	APP_SECRET: 'a0jox0m0kxfLPc7bYOl9A3OKuPoDmLOcxgFVxHmj9hJuObFPJO'
 };
+
